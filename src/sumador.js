d99d60d26ebedea57695b5cbf7e4b285007beaae
Nicolas <nicolascari.rz@gmail.com>
2022-09-19 18:20:42 -0400

    un usuario puede dar un delimitador, usando regex como sintaxis

--- src/sumador.js
+++ src/sumador.js
@@ -1,12 +1,35 @@
+
+let cadenas = [",","-"].join("|")
+
+function aumentarDelimitadores(str)
+{
+  const splitArr = str.split(" ");
+  var primerDelimitadorRegex = splitArr[0].toString();
+
+  let cadenaDelimitadores = primerDelimitadorRegex.substr(0, primerDelimitadorRegex.length-1);
+  let delim = cadenaDelimitadores.substr(-1, 1);
+  return delim
+}
+
 function sumar(str) {
   if (str == null)
   {
     return 0
   }
 
-  var regex = new RegExp(",");
-  const splitArr = str.split(regex);
   let tot = 0
+  let cadenasUsuario
+  if(str[0] == "/")
+  {
+    const parteNumeros = str.split(" ");
+    str = parteNumeros[1].toString();
+    cadenasUsuario = cadenas + "|" + aumentarDelimitadores(str)
+  }
+  else{
+    cadenasUsuario = cadenas
+  }
+  var regex = new RegExp(cadenasUsuario);
+  const splitArr = str.split(regex);
   for (let i = 0; i < splitArr.length; i++) {
     if (isNaN(splitArr[i]) == true)
     {
