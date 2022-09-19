3da32c620ce66d90d43e2a51da9274a410697d76
Nicolas <nicolascari.rz@gmail.com>
2022-09-19 11:27:26 -0400

    arreglando codigo chueco, usando split y regex para mejor funcionalidad al anadir nuevos parametros

--- src/sumador.js
+++ src/sumador.js
@@ -1,56 +1,21 @@
-let separadorCustom
-
-function compararSeparador(char)
-{
-  let pertenece = false
-  if (char == ",")
-  {
-    pertenece = true
-  }
-  if (char == "-"){
-    pertenece = true
-  }
-  if (char == " ")
-  {
-    pertenece = true
-  }
-  if (char == separadorCustom){
-    pertenece = true
-  }
-
-  return pertenece
-}
-
-
 function sumar(str) {
   if (str == null)
   {
     return 0
   }
+
+  var regex = new RegExp(",");
+  const splitArr = str.split(regex);
   let tot = 0
-  for (let i = 0; i < str.length; i++) {
-    let num = parseInt(str[i]);
-    if (isNaN(str[i]) == false)
+  for (let i = 0; i < splitArr.length; i++) {
+    if (isNaN(splitArr[i]) == true)
     {
-      tot = tot + num
+      return "cadena no valida"
     }
-    else
-    {
-      if(str[i] == "/")
-      {
-         separadorCustom = str[i+3]
-         i += 5
-      }
-
-      if (compararSeparador(str[i]) == false)
-      {
-        return "cadena no valida"
-      }
-
-    }
+    let num = parseInt(splitArr[i]);
+    tot += num
   }
-
-  return tot;
+  return tot
 }
 
 export {sumar};
